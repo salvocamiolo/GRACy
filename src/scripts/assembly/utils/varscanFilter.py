15ce8b697e7509a66a8264222d40b2606f102848
salvocamiolo <salvocamiolo@gmail.com>
2020-06-02 11:30:26 +0100

    improved varscan filtering

--- src/scripts/assembly/utils/varscanFilter.py
+++ src/scripts/assembly/utils/varscanFilter.py
@@ -54,10 +54,10 @@ while True:
 	if validate == 1:
 		if len(fields[4])>1: #a deletion
 			print("Validating deletion at position %s" %fields[1])
-			print(refSeq[int(fields[1])-25:int(fields[1])-1]+"-"+fields[3]+"-"+refSeq[int(fields[1]):int(fields[1])+26-len(fields[3])])
-			print(refSeq[int(fields[1])-25:int(fields[1])-1]+"-"+fields[4]+"-"+refSeq[int(fields[1]):int(fields[1])+26-len(fields[4])])
-			refAllele = refSeq[int(fields[1])-25:int(fields[1])-1]+fields[3]+refSeq[int(fields[1]):int(fields[1])+26-len(fields[3])]
-			altAllele = refSeq[int(fields[1])-25:int(fields[1])-1]+fields[4]+refSeq[int(fields[1]):int(fields[1])+26-len(fields[4])]
+			print(refSeq[int(fields[1])-25:int(fields[1])-1]+"-"+fields[3]+"-"+refSeq[int(fields[1]):int(fields[1])+25])
+			print(refSeq[int(fields[1])-25:int(fields[1])-1]+"-"+fields[4]+"-"+refSeq[int(fields[1]):int(fields[1])+25-len(fields[4])+1])
+			refAllele = refSeq[int(fields[1])-25:int(fields[1])-1]+fields[3]+refSeq[int(fields[1]):int(fields[1])+25]
+			altAllele = refSeq[int(fields[1])-25:int(fields[1])-1]+fields[4]+refSeq[int(fields[1]):int(fields[1])+25-len(fields[4])+1]
 			refReads = kmersInReads.count(refAllele) + kmersInReads.count(Seq.reverse_complement(refAllele))
 			altReads = kmersInReads.count(altAllele) + kmersInReads.count(Seq.reverse_complement(altAllele))
 
