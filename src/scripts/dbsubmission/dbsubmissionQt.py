0f83b8d83505efbe78b096b194a9d3423b83e9ed
salvocamiolo <salvocamiolo@gmail.com>
2020-06-04 10:18:45 +0100

    added new webin-cli tool

--- src/scripts/dbsubmission/dbsubmissionQt.py
+++ src/scripts/dbsubmission/dbsubmissionQt.py
@@ -504,7 +504,7 @@ class Ui_Form(object):
 					#os.system("rm -f "+fq1+" "+fq2)
 					#print "Waiting ENA output...."
 					#time.sleep(10)
-					os.system("grep . fastqReceipt > tempF ; mv tempF fastqReceipt")
+					os.system("sed -i '/processed/d' fastqReceipt")
 					receiptFile = open("fastqReceipt")
 					while True:
 						line = receiptFile.readline().rstrip()
